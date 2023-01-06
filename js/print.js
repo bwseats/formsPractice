e13374d276a8884144d3647f58f685ea5e5810da
benjamin seats <benjiwyattx@gmail.com>
2023-01-05 22:21:14 -0500

    fixed invoice page

--- js/print.js
+++ js/print.js
@@ -1,6 +1,12 @@
 function printpage() {
-    var printButton = document.getElementById("printbutton");
+    let printButton = document.getElementById("printButton");
+    let navbar = document.getElementById("navbar");
+    let footer = document.getElementById("footer");
     printButton.style.visibility = 'hidden';
+    navbar.style.visibility = 'hidden';
+    footer.style.visibility = 'hidden';
     window.print()
     printButton.style.visibility = 'visible';
+    navbar.style.visibility = 'visible';
+    footer.style.visibility = 'visible';
 }
